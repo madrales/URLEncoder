f978a99630604a0e2c4cb3e1f1c4d7a0f3c8a546
Mason Adrales <masonadrales@mwc-105247.dhcp.missouri.edu>
2019-02-17 14:10:41 -0600

    First Commit

---
+++ URLEncoder/URLEncoder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace URLEncoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello! Welcome to the URL Encoding Program.");
+                Console.WriteLine("First, let's begin by getting some information. What is the Project Name?");
+                string projectName = Console.ReadLine();
+                Console.WriteLine("Awesome! Now, what is the Activity Name?");
+                string activityName = Console.ReadLine();
+                Console.WriteLine("Okay! Here is your URL:");
+                Console.WriteLine("https://companyserver.com/content/" + projectName + "/files/" + activityName + "/" + activityName + "Report.pdf");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
