aa9e69c371d3a6502a9243be26776906cc188ea3
Mason Adrales <masonadrales@mwc-107136.dhcp.missouri.edu>
2019-02-22 13:21:45 -0600

    New changes to file

---
+++ DaleSolution/DaleSolution/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace URLEncoder
+{
+    class Program
+    {
+        static string urlFormatString = "https://companyserver.com/content/{0}/files/{1}/{1}Report.pdf";
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("URL Encoder");
+
+            do
+            {
+                Console.Write("\nProject name: ");
+                string projectName = GetUserInput();
+                Console.Write("Activity name: ");
+                string activityName = GetUserInput();
+
+                Console.WriteLine(CreateURL(projectName, activityName));
+
+                Console.Write("Would you like to do another? (y/n): ");
+            } while (Console.ReadLine().ToLower().Equals("y"));
+        }
+
+        static string CreateURL(string projectName, string activityName)
+        {
+            Console.WriteLine("https://companyserver.com/content/" + projectName + "/files/" + activityName + "/" + activityName + "Report.pdf");
+        }
+
+        static string GetUserInput()
+        {
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+                if (IsValid(input)) return input;
+                Console.WriteLine("The input contains invaldi characters. Enter again: ");
+            } while (true);
+        }
+
+        static bool IsValid(string input)
+        {
+            foreach (char character in input.ToCharArray())
+            {
+                if (character == 0x1F)
+                {
+                    Console.WriteLine("Sorry! Hexadecimal numbers are not allowed. Try Again.");
+                }
+
+            }
+        }
+
+        static string Encode(string value)
+        {
+            string encodedValue = "";
+            foreach (char character in value.ToCharArray())
+            {
+
+            }
+        }
+    }
+}
--- URLEncoder/URLEncoder/Program.cs
+++ URLEncoder/URLEncoder/Program.cs
@@ -4,22 +4,120 @@ namespace URLEncoder
 {
     class Program
     {
+        static bool flag = false;
+        public static string[] controls = { "?", "/", "@", "&", ";", ":", "$", "+", "=" };
         static void Main(string[] args)
         {
-            try
+            do
             {
-                Console.WriteLine("Hello! Welcome to the URL Encoding Program.");
-                Console.WriteLine("First, let's begin by getting some information. What is the Project Name?");
-                string projectName = Console.ReadLine();
-                Console.WriteLine("Awesome! Now, what is the Activity Name?");
-                string activityName = Console.ReadLine();
-                Console.WriteLine("Okay! Here is your URL:");
-                Console.WriteLine("https://companyserver.com/content/" + projectName + "/files/" + activityName + "/" + activityName + "Report.pdf");
+                try
+                {
+                    Console.WriteLine("Hello! Welcome to the URL Encoding Program.");
+                    Console.WriteLine("First, let's begin by getting some information. What is the Project Name?");
+                    string projectName = GetUserInput();
+                    Console.WriteLine("Awesome! Now, what is the Activity Name?");
+                    string activityName = GetUserInput();
+                    Console.WriteLine("Okay! Here is your URL:");
+                    Console.WriteLine("https://companyserver.com/content/" + projectName + "/files/" + activityName + "/" + activityName + "Report.pdf");
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter valid details.");
+                }
+                Console.WriteLine("\n");
+                Console.WriteLine("Would you like to create another URL?");
+                Console.WriteLine("Y/N");
+                char response = Convert.ToChar(Console.ReadLine());
+                if (response == 'y')
+                {
+                    flag = true;
+                }
+                if (response == 'n')
+                {
+                    flag = false;
+                }
+            } while (flag == true);
+        }
+        static string GetUserInput()
+        {
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+                if (IsValid(input)) return input;
+                Console.Write("This input doesn't work.");
+            } while (true);
+        }
+        static bool IsValid(string input)
+        {
+            foreach (char character in input.ToCharArray())
+            {
+                if (input.ToLower().Contains("?"))
+                {
+                    Console.WriteLine("Invalid input.");
+                    return false;
+                }
+                if (input.ToLower().Contains("/"))
+                {
+                    Console.WriteLine("Invalid input.");
+                    return false;
+                }
+                if (input.ToLower().Contains("$"))
+                {
+                    Console.WriteLine("Invalid input.");
+                    return false;
+                }
+                if (input.ToLower().Contains("="))
+                {
+                    Console.WriteLine("Invalid input.");
+                    return false;
+                }
+                if (input.ToLower().Contains("+"))
+                {
+                    Console.WriteLine("Invalid input.");
+                    return false;
+                }
+                if (input.ToLower().Contains("&"))
+                {
+                    Console.WriteLine("Invalid input.");
+                    return false;
+                }
+                if (input.ToLower().Contains("@"))
+                {
+                    Console.WriteLine("Invalid input.");
+                    return false;
+                }
+                if (input.ToLower().Contains(":"))
+                {
+                    Console.WriteLine("Invalid input.");
+                    return false;
+                }
+                if (input.ToLower().Contains(";"))
+                {
+                    Console.WriteLine("Invalid input.");
+                    return false;
+                }
+                if (character == 0x1F)
+                {
+                    Console.WriteLine("Hexadecimals can not be accepted. Sorry!");
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Input given deemed valid.");
+                    return true;
+                }
             }
-            catch (Exception e)
+            return true;
+        }
+        static string Encode(string value)
+        {
+            string encodedValue = "";
+            foreach (char character in value.ToCharArray())
             {
-                Console.WriteLine("Exception: " + e.Message);
+                
             }
         }
     }
 }
+// "/", "@", "&", ";", ":", "$", "+", "="
